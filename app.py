e8ed51b88096e2b15ac769ae1333e573a392f89f
474908647@qq.com <kimulsanne@HuJintaodeMacBook-Pro.local>
2018-06-28 09:52:31 +0800

    fa

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def hello():
     except RedisError:
         visits = "<i>cannot connect to Redis, counter disabled</i>"
 
-    html = "<h3>Test Hello {name}!</h3>" \
+    html = "<h3>This is Test Hello {name}!</h3>" \
         "<b>Hostname:</b> {hostname}<br />" \
         "<b>Visits:</b> {visits}"
 
